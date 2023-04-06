8897faf5babe96abc3cc42a9adcb5bd8822dca9f
Achilleas Tsimichodimos <achillestsimiho@gmail.com>
2023-04-06 15:51:59 +0300

    c++ it is

--- ocaml_parser/Main.ml
+++
@@ -1,8 +0,0 @@
-let main =
-  let lexbuf = Lexing.from_channel stdin in
-  try
-    Parser.program Lexer.lexer lexbuf;
-    exit 0
-  with Parsing.Parse_error ->
-    Printf.eprintf "syntax error\n";
-    exit 1
