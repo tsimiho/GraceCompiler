452a53cc079c4692577b39da622a853162f3ff9c
Achilleas Tsimichodimos <achilleas.tsimichodimos@int.twi.gr>
2023-10-28 19:04:17 +0300

    cpp bonus pack

---
+++ bonus/error.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *  CVS version:
+ *     $Id: error.h,v 1.1 2003/05/13 22:21:01 nickie Exp $
+ ******************************************************************************
+ *
+ *  C header file : error.h
+ *  Project       : PCL Compiler
+ *  Version       : 1.0 alpha
+ *  Written by    : Nikolaos S. Papaspyrou (nickie@softlab.ntua.gr)
+ *  Date          : May 14, 2003
+ *  Description   : Generic symbol table in C, simple error handler
+ *
+ */
+
+
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+
+/* ---------------------------------------------------------------------
+   ---------------------------------------------------------------------
+   --------------------------------------------------------------------- */
+
+void internal (const char * fmt, ...);
+void fatal    (const char * fmt, ...);
+void error    (const char * fmt, ...);
+void warning  (const char * fmt, ...);
+
+
+#endif
