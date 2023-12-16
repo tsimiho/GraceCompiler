63b770b5f63cce3651ffc11f3aee0acaad79ee57
Achilleas Tsimichodimos <achillestsimiho@gmail.com>
2023-12-16 14:00:57 +0200

    small changes

--- bonus/error.h
+++ bonus/error.h
@@ -12,19 +12,16 @@
  *
  */
 
-
 #ifndef __ERROR_H__
 #define __ERROR_H__
-
 
 /* ---------------------------------------------------------------------
    ---------------------------------------------------------------------
    --------------------------------------------------------------------- */
 
-void internal (const char * fmt, ...);
-void fatal    (const char * fmt, ...);
-void error    (const char * fmt, ...);
-void warning  (const char * fmt, ...);
-
+void internal(const char *fmt, ...);
+void fatal(const char *fmt, ...);
+void error(const char *fmt, ...);
+void warning(const char *fmt, ...);
 
 #endif
--- interpreter/ast.hpp
+++ interpreter/ast.hpp
@@ -3,242 +3,278 @@
 
 #include <iostream>
 #include <map>
-#include <vector>
 #include <string>
 #include <variant>
+#include <vector>
 
 #include "symbol.hpp"
 
 void yyerror(const char *msg);
 
-inline std::ostream& operator<<(std::ostream &out, Type t) {
-    switch (t) {
-        case TYPE_int: out << "int"; break;
-        case TYPE_char: out << "char"; break;
-    }
-    return out;
+inline std::ostream &operator<<(std::ostream &out, Type t) {
+  switch (t) {
+  case TYPE_int:
+    out << "int";
+    break;
+  case TYPE_char:
+    out << "char";
+    break;
+  }
+  return out;
 }
 
 class AST {
 public:
-    virtual ~AST() {}
-    virtual void printOn(std::ostream &out) const = 0;
-    virtual void sem() {}
+  virtual ~AST() {}
+  virtual void printOn(std::ostream &out) const = 0;
+  virtual void sem() {}
 };
 
-inline std::ostream& operator<< (std::ostream &out, const AST &t) {
-    t.printOn(out);
-    return out;
+inline std::ostream &operator<<(std::ostream &out, const AST &t) {
+  t.printOn(out);
+  return out;
 }
 
-class Expr: public AST {
+class Expr : public AST {
 public:
-    virtual std::variant<int, char> eval() const = 0;
-    void type_check(Type t) {
-        sem();
-        if (type != t) yyerror("Type mismatch");
-    }
+  virtual std::variant<int, char> eval() const = 0;
+  void type_check(Type t) {
+    sem();
+    if (type != t)
+      yyerror("Type mismatch");
+  }
+
 protected:
-    Type type;
+  Type type;
 };
 
-class Stmt: public AST {
+class Stmt : public AST {
 public:
-    virtual void run() const = 0;
+  virtual void run() const = 0;
 };
 
 extern std::vector<int> rt_stack;
 
 // Expressions
 
-class Id: public Expr {
+class Id : public Expr {
 public:
-    Id(char v): var(v), offset(-1) {}
-    virtual void printOn(std::ostream &out) const override {
-        out << "Id(" << var << "@" << offset << ")";
-    }
-    virtual std::variant<int, char> eval() const override {
-        return rt_stack[offset];
-    }
-    virtual void sem() override {
-        SymbolEntry *e = st.lookup(var);
-        type = e->type;
-        offset = e->offset;
-    }
+  Id(char v) : var(v), offset(-1) {}
+  virtual void printOn(std::ostream &out) const override {
+    out << "Id(" << var << "@" << offset << ")";
+  }
+  virtual std::variant<int, char> eval() const override {
+    return rt_stack[offset];
+  }
+  virtual void sem() override {
+    SymbolEntry *e = st.lookup(var);
+    type = e->type;
+    offset = e->offset;
+  }
+
 private:
-    char var;
-    int offset;
+  char var;
+  int offset;
 };
 
-class Const: public Expr {
+class Const : public Expr {
 public:
-    Const(int n): num(n) {}
-    virtual void printOn(std::ostream &out) const override {
-        out << "IntConst(" << num << ")";
-    }
-    virtual std::variant<int, char> eval() const override {
-        return num;
-    }
-    virtual void sem() override {
-        type = TYPE_int;
-    }
+  Const(int n) : num(n) {}
+  virtual void printOn(std::ostream &out) const override {
+    out << "IntConst(" << num << ")";
+  }
+  virtual std::variant<int, char> eval() const override { return num; }
+  virtual void sem() override { type = TYPE_int; }
+
 private:
-    int num;
+  int num;
 };
 
-class CharConst: public Expr {
+class CharConst : public Expr {
 public:
-    CharConst(char c): ch(c) {}
-    virtual void printOn(std::ostream &out) const override {
-        out << "CharConst(" << ch << ")";
-    }
-    virtual std::variant<int, char> eval() const override {
-        return ch;
-    }
-    virtual void sem() override {
-        type = TYPE_char;
-    }
+  CharConst(char c) : ch(c) {}
+  virtual void printOn(std::ostream &out) const override {
+    out << "CharConst(" << ch << ")";
+  }
+  virtual std::variant<int, char> eval() const override { return ch; }
+  virtual void sem() override { type = TYPE_char; }
+
 private:
-    char ch;
+  char ch;
 };
 
-class BinOp: public Expr {
+class BinOp : public Expr {
 public:
-    BinOp(Expr *l, std::string o, Expr *r): left(l), op(o), right(r) {}
-    ~BinOp() { delete left; delete right; }
-    virtual void printOn(std::ostream &out) const override {
-        out << op << "(" << *left << ", " << *right << ")";
+  BinOp(Expr *l, std::string o, Expr *r) : left(l), op(o), right(r) {}
+  ~BinOp() {
+    delete left;
+    delete right;
+  }
+  virtual void printOn(std::ostream &out) const override {
+    out << op << "(" << *left << ", " << *right << ")";
+  }
+  virtual std::variant<int, char> eval() const override {
+    switch (op) {
+    case "+":
+      return left->eval() + right->eval();
+    case "-":
+      return left->eval() - right->eval();
+    case "*":
+      return left->eval() * right->eval();
+    case "div":
+      return left->eval() / rigth->eval();
+    case "mod":
+      return left->eval() % rigth->eval();
     }
-    virtual std::variant<int, char> eval() const override {
-        switch (op) {
-            case "+":   return left->eval() + right->eval();
-            case "-":   return left->eval() - right->eval();
-            case "*":   return left->eval() * right->eval();
-            case "div": return left->eval() / rigth->eval();
-            case "mod": return left->eval() % rigth->eval();
-        }
-        return 42;  // this will never be reached.
+    return 42; // this will never be reached.
+  }
+  virtual void sem() override {
+    left->type_check(TYPE_int);
+    right->type_check(TYPE_int);
+    switch (op) {
+    case "+":
+    case "-":
+    case "*":
+    case "div":
+    case "mod":
+      type = TYPE_int;
+      break;
     }
-    virtual void sem() override {
-        left->type_check(TYPE_int);
-        right->type_check(TYPE_int);
-        switch (op) {
-            case "+": case "-": case "*": case "div": case "mod":
-                type = TYPE_int; break;
-        }
-    }
+  }
+
 private:
-    Expr *left;
-    std::string op;
-    Expr *right;
+  Expr *left;
+  std::string op;
+  Expr *right;
 };
 
-class Cond: public Expr {
+class Cond : public Expr {
 public:
-    Cond(Expr *l, std::string o, Expr *r): left(l), op(o), right(r) {}
-    ~Cond() { delete left; delete right; }
-    virtual void printOn(std::ostream &out) const override {
-        out << op << "(" << *left << ", " << *right << ")";
+  Cond(Expr *l, std::string o, Expr *r) : left(l), op(o), right(r) {}
+  ~Cond() {
+    delete left;
+    delete right;
+  }
+  virtual void printOn(std::ostream &out) const override {
+    out << op << "(" << *left << ", " << *right << ")";
+  }
+  virtual bool eval() const override {
+    switch (op) {
+    case "=":
+      return left->eval() == right->eval();
+    case "#":
+      return left->eval() != right->eval();
+    case "<":
+      return left->eval() < right->eval();
+    case ">":
+      return left->eval() > rigth->eval();
+    case "<=":
+      return left->eval() <= rigth->eval();
+    case ">=":
+      return left->eval() >= rigth->eval();
     }
-    virtual bool eval() const override {
-        switch (op) {
-            case "=":  return left->eval() == right->eval();
-            case "#":  return left->eval() != right->eval();
-            case "<":  return left->eval() < right->eval();
-            case ">":  return left->eval() > rigth->eval();
-            case "<=": return left->eval() <= rigth->eval();
-            case ">=": return left->eval() >= rigth->eval();
-        }
-        return 42;  // this will never be reached.
-    }
+    return 42; // this will never be reached.
+  }
+
 private:
-    Expr *left;
-    std::string op;
-    Expr *right;
+  Expr *left;
+  std::string op;
+  Expr *right;
 };
 
 // Statements
 
-class Assign: public Stmt {
+class Assign : public Stmt {
 public:
-    Assign(LValue *l, Expr *e): lval(l), expr(e) {}
-    ~Assign() { delete lval; delete expr; }
-    virtual void printOn(std::ostream &out) const override {
-        out << "Assign(" << *lval << ", " << *expr << ")";
-    }
-    virtual void run() const override {
-        SymbolEntry *lhs = st.lookup(var);
-        expr->type_check(lhs->type);
-        offset = lhs->offset;
-    }
-    virtual void sem() override {
+  Assign(LValue *l, Expr *e) : lval(l), expr(e) {}
+  ~Assign() {
+    delete lval;
+    delete expr;
+  }
+  virtual void printOn(std::ostream &out) const override {
+    out << "Assign(" << *lval << ", " << *expr << ")";
+  }
+  virtual void run() const override {
+    SymbolEntry *lhs = st.lookup(var);
+    expr->type_check(lhs->type);
+    offset = lhs->offset;
+  }
+  virtual void sem() override {}
 
-    }
 private:
-    LValue *lval;
-    int offset;
-    Expr *expr;
+  LValue *lval;
+  int offset;
+  Expr *expr;
 };
 
-class If: public Stmt {
+class If : public Stmt {
 public:
-    If(Expr *c, Stmt *s1, Stmt *s2 = nullptr):
-        cond(c), stmt1(s1), stmt2(s2) {}
-    ~If() { delete cond; delete stmt1; delete stmt2; }
-    virtual void printOn(std::ostream &out) const override {
-        out << "If(" << *cond << ", " << *stmt1;
-        if (stmt2 != nullptr) out << ", " << *stmt2;
-        out << ")";
-    }
-    virtual void run() const override {
-        if (cond->eval())
-            stmt1->run();
-        else if (stmt2 != nullptr)
-            stmt2->run();
-    }
-    virtual void sem() override {
-        // cond->type_check(TYPE_char);
-        stmt1->sem();
-        if (stmt2 != nullptr) stmt2->sem();
-    }
+  If(Expr *c, Stmt *s1, Stmt *s2 = nullptr) : cond(c), stmt1(s1), stmt2(s2) {}
+  ~If() {
+    delete cond;
+    delete stmt1;
+    delete stmt2;
+  }
+  virtual void printOn(std::ostream &out) const override {
+    out << "If(" << *cond << ", " << *stmt1;
+    if (stmt2 != nullptr)
+      out << ", " << *stmt2;
+    out << ")";
+  }
+  virtual void run() const override {
+    if (cond->eval())
+      stmt1->run();
+    else if (stmt2 != nullptr)
+      stmt2->run();
+  }
+  virtual void sem() override {
+    // cond->type_check(TYPE_char);
+    stmt1->sem();
+    if (stmt2 != nullptr)
+      stmt2->sem();
+  }
+
 private:
-    Expr *cond;
-    Stmt *stmt1;
-    Stmt *stmt2;
+  Expr *cond;
+  Stmt *stmt1;
+  Stmt *stmt2;
 };
 
-class While: public Stmt {
+class While : public Stmt {
 public:
-    While(Cond *c, Stmt *s): cond(c), stmt(s) {}
-    ~While() { delete cond; delete stmt; }
-    virtual void printOn(std::ostream &out) const override {
-        out << "While(" << *expr << ", " << *stmt << ")";
+  While(Cond *c, Stmt *s) : cond(c), stmt(s) {}
+  ~While() {
+    delete cond;
+    delete stmt;
+  }
+  virtual void printOn(std::ostream &out) const override {
+    out << "While(" << *expr << ", " << *stmt << ")";
+  }
+  virtual void run() const override {
+    while (cond->eval()) {
+      stmt->run();
     }
-    virtual void run() const override {
-        while (cond->eval()) {
-            stmt->run();
-        }
-    }
+  }
+
 private:
-    Cond *cond;
-    Stmt *stmt;
+  Cond *cond;
+  Stmt *stmt;
 };
 
-class Return: public Stmt {
+class Return : public Stmt {
 public:
-    Return(Expr *e = nullptr): expr(e) {}
-    ~Return() { delete expr; }
-    virtual void printOn(std::ostream &out) const override {
-        out << "Return(";
-        if (stmt2 != nullptr) out << *stmt2;
-        out << ")";
-    }
-    virtual void run() const override {
-        return expr->eval();
-    }
-private:
-    Expr *expr;
-};
+  Return(Expr *e = nullptr) : expr(e) {}
+  ~Return() { delete expr; }
+  virtual void printOn(std::ostream &out) const override {
+    out << "Return(";
+    if (stmt2 != nullptr)
+      out << *stmt2;
+    out << ")";
+  }
+  virtual void run() const override { return expr->eval(); }
 
+private:
+  Expr *expr;
+};
 
 #endif
